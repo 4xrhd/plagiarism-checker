14e1677d5ee714a7cb43ac2f5e04a213e46733f6
Muhammet Ezhar Eddin <65583096+4xrhd@users.noreply.github.com>
2024-04-29 14:06:53 +0600

    updated browser opeing issue

--- plagiarism-checker.py
+++ plagiarism-checker.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, render_template
 import re
 import math
+import webbrowser
+
 
 app = Flask(__name__)
 
@@ -58,4 +60,6 @@ def cosineSimilarity():
         return render_template('index.html', query=inputQuery, output=output)
 
 if __name__ == "__main__":
+    webbrowser.open("http://127.0.0.1:5000")
     app.run()
+    
